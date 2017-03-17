ad317e92fcc8f4391e7ad7203f1b413765934e19
Phill <phill@elaines-MacBook-Pro-2.local>
2017-03-17 08:46:03 -0700

    added table

---
+++ MultiColor.swift
@@ -0,0 +1,9 @@
+//
+//  MultiColor.swift
+//  maps
+//
+//  Created by Phill on 3/16/17.
+//  Copyright © 2017 Phill. All rights reserved.
+//
+
+import Foundation
---
+++ maps/MulticolorPolylineSegment.swift
@@ -0,0 +1,13 @@
+//
+//  MulticolorPolylineSegment.swift
+//  maps
+//
+//  Created by Phill on 3/17/17.
+//  Copyright © 2017 Phill. All rights reserved.
+//
+
+import UIKit
+
+class MulticolorPolylineSegment: MKPolyline {
+
+}
